287c45480712f6cdd82b0bdedc5a15bda11ecf95
Jegathesan Shanmugam <nullbyte.in@gmail.com>
2021-07-23 20:54:18 +0530

    Enabled GPU and SSH

--- turtlebot3/install_turtlebot3.bash
+++ turtlebot3/install_turtlebot3.bash
@@ -15,7 +15,7 @@ function dep_install()
 
     # ROS Dep
     python -m pip install msgpack argparse rosinstall empy defusedxml netifaces --user
-    python -m pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.8.0-cp27-none-linux_x86_64.whl --user
+    python -m pip install tensorflow-gpu==1.15.0 --user
     python -m pip install keras==2.1.5 --user
     python -m pip install tensorboard --user
 
